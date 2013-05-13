237866fe060da68ce8cf01280faaa2afc2b9e6e6
jgdimeglio@gmail.com <jgdimeglio@gmail.com@b1a46512-3d6b-d0f3-87d7-4616227a872c>
2013-05-13 20:58:52 +0000

    clock

---
+++ so-proyect/system/clock.py
@@ -0,0 +1,41 @@
+'''
+Created on 13/05/2013
+
+@author: Di Meglio
+'''
+import threading  as t
+import time
+
+class Clock(t.thread):
+    
+    def _init_(self,cpu,timer):
+        self.cpu=cpu;
+        self.timer=timer
+    
+    def run(self):
+        while(True):
+            time.sleep(1)
+            self.timer.click()
+            
+            
+class TimerQuantum():
+    
+    def _init_(self,quantum):
+        self.quantum=quantum
+        self.currentTime=0
+        
+    def click(self,cpu):
+        if(self.quantum>self.currentTime):
+            cpu.click()
+            self.currentTime+=1
+        else:
+            self.currentTime=0
+            
+        
+        
+        
+class Timer():
+        
+    def click(self,cpu):
+        cpu.click()
+    
